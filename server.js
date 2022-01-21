f8f1c5b75fe26179e648e4cc7cb7d021c5fde456
Jaewon Youn <yjw970521@gmail.com>
2022-01-21 21:41:07 +0900

    login controller login form

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var swtoolRouter = require('./routes/SwtoolRout.js');
 var fileuploadRouter = require('./routes/UploadRout');
+var userRouter = require('./routes/UsersRout');
 
 var app = express();
 
@@ -12,6 +13,7 @@ app.use('/users', usersRouter);
 app.use('/api/Swtool', swtoolRouter);
 app.use('/api/upload', fileuploadRouter);
 app.use(express.static("./uploads"));
+app.use("/api/register", userRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
