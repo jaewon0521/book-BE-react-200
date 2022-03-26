1b214846b4c7acd8919feccf5fc1b7eedc38c07b
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-03-26 16:50:36 +0900

    fixed file

--- ignorefile/awsAuth.js
+++
@@ -1,8 +0,0 @@
-let auth = {
-  user: "younjaewon",
-  password: "Mydbjaewon970521",
-  host: "mydbjaewon.cjncfvodiaus.us-east-2.rds.amazonaws.com",
-  database: "react",
-};
-
-module.exports = auth
